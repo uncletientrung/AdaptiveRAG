555ecc204a4c2e5e12cf0402b1b146f6fe9dafa1
uncletientrung <thihachcf@gmail.com>
2026-04-23 21:22:24 +0700

    Them filter metadata khi filter search

--- rag/hybrid_retriever.py
+++ rag/hybrid_retriever.py
@@ -1,12 +1,43 @@
 from langchain.retrievers import EnsembleRetriever
 from langchain_community.retrievers import BM25Retriever
 from rag.retriever import get_retriever
+from langchain.schema import BaseRetriever
+from typing import Any, Optional
+
+class FilteredVectorRetriever(BaseRetriever):
+    vectorstore: Any
+    k: int
+    fetch_k: int
+    filter_metadata: Optional[dict] = None
+
+    def _get_relevant_documents(self, query, *, run_manager=None):
+        docs = self.vectorstore.similarity_search(query, k=self.fetch_k)
+
+        if self.filter_metadata:
+            docs = [
+                d for d in docs
+                if d.metadata.get("file_name") == self.filter_metadata.get("file_name")
+            ]
+
+        return docs[:self.k]
 
 def create_hybrid_retriever(vectorstore, chunks, top_k= 3, fetch_k=20, bm25_weight=0.35, vector_weight=0.65, filter_metadata =None):
-    vector_retriever = get_retriever(vectorstore, k=top_k, fetch_k=fetch_k) # semantic search
+    if filter_metadata:
+        chunks = [ doc for doc in chunks
+                    if doc.metadata.get("file_name") == filter_metadata.get("file_name")]
+    if not chunks: 
+        chunks = []
 
     bm25_retriever = BM25Retriever.from_documents(chunks) # keyword search
     bm25_retriever.k = top_k
+    
+    # vector_retriever = get_retriever(vectorstore, k=top_k, fetch_k=fetch_k) # semantic search
+    vector_retriever = FilteredVectorRetriever(
+        vectorstore=vectorstore,
+        k=top_k,
+        fetch_k=fetch_k,
+        filter_metadata=filter_metadata
+    )
 
     combine_Search = EnsembleRetriever(
         retrievers=[bm25_retriever, vector_retriever],
--- rag/retriever.py
+++ rag/retriever.py
@@ -1,12 +1,12 @@
 from langchain_community.vectorstores import FAISS
 from typing import Optional
 
-def get_retriever(vectorstore: FAISS, k: int = 3, fetch_k: int = 20,  filter=None): # fetch_k là số vector để chọn top k
+def get_retriever(vectorstore: FAISS, k: int = 3, fetch_k: int = 20,  filter_metadata=None): # fetch_k là số vector để chọn top k
     return vectorstore.as_retriever( # Gán FAISS thành retriever
         search_type="similarity", # Tìm các vector gần query nhất
         search_kwargs={
             "k": k,
             "fetch_k": fetch_k,
-            "filter": filter   # KEY POINT
         }
     )
+# as_retriever() của FAISS trong LangChain KHÔNG hỗ trợ filter_metadata
